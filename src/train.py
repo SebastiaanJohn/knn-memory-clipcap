e0af2fb46bc288b637af8bbd0c0cf87e81b01d1f
Sebastiaan Dijkstra <sebastiaandijkstra@gmail.com>
2023-05-18 13:09:47 +0200

    fix

--- src/train.py
+++ src/train.py
@@ -1,7 +1,6 @@
 """Training script for CLIPCAP model."""
 
 import argparse
-import json
 import logging
 import os
 import sys
@@ -17,16 +16,7 @@ from transformers import (
     AdamW,
     get_linear_schedule_with_warmup,
 )
-
-
-def save_config(args: argparse.Namespace) -> None:
-    """Save the config to a file."""
-    config = {}
-    for key, item in args._get_kwargs():
-        config[key] = item
-    out_path = os.path.join(args.out_dir, f"{args.prefix}.json")
-    with open(out_path, "w") as outfile:
-        json.dump(config, outfile)
+from utils import save_config
 
 
 def train(
